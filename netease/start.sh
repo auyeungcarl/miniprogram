1a5e6e6c26f1f366022e8fe54f0df91e15e57199
auyeungcarl <838644308ouyc@gmail.com>
2023-08-13 01:12:54 +0800

    修改日志

--- netease/start.sh
+++ netease/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build -t netease:1.0 .
-docker run -d -p 8001:8082 netease:1.0
+docker run -d -p 8001:8082 -v /home/ouyc/java/netease/logs:/home  -v /home/ouyc/java/netease/timezone:/etc/timezone -v /etc/localtime:/etc/localtime  netease:1.0
 
 echo "============================================================================"
 docker images | grep REPOSITORY
@@ -12,7 +12,7 @@ docker container list  | grep CONTAINER
 docker container list |grep netease |grep 1.0
 
 echo "查看日志、登录容器的指令如下："
-echo "docker logs -tf $(docker container list |grep netease |grep 1.0 | awk '{print $1}') "
+echo "docker logs -tf --tail=100 $(docker container list |grep netease |grep 1.0 | awk '{print $1}') "
 echo "docker exec -it  $(docker container list |grep netease |grep 1.0 | awk '{print $1}') /bin/sh"
 
 echo "start finish"
