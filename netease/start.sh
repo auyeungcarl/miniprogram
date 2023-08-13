d7f2538b8b20b4b7556180f2ad9e3031996bfd25
auyeungcarl <838644308ouyc@gmail.com>
2023-08-13 11:14:30 +0800

    修改启动脚本

--- netease/start.sh
+++ netease/start.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-docker build -t netease:1.0 .
-docker run -d -p 8001:8082 -v /home/ouyc/java/netease/logs:/home  -v /home/ouyc/java/netease/timezone:/etc/timezone -v /etc/localtime:/etc/localtime  netease:1.0
+imageName=netease
+tag=1.0
+
+dockerPort=8082
+hostPort=8001
+
+dockerLogPath=/home
+hostLogPath=/home/ouyc/java/netease/logs
+
+dockerTimeZoneFile=/etc/timezone
+hostTimeZoneFile=/home/ouyc/java/netease/timezone
+
+dockerLocalTime=/etc/localtime
+
+hostLocalTime=/etc/localtime
+
+docker build -t ${imageName}:${tag} .
+docker run -d -p ${hostPort}:${dockerPort} -v ${hostLogPath}:${dockerLogPath}  -v ${hostTimeZoneFile}:${dockerTimeZoneFile} -v ${hostLocalTime}:${dockerLocalTime}  ${imageName}:${tag}
 
 echo "============================================================================"
 docker images | grep REPOSITORY
-docker images | grep netease|grep 1.0
+docker images | grep ${imageName}|grep ${tag}
 
 echo "============================================================================"
 docker container list  | grep CONTAINER
-docker container list |grep netease |grep 1.0
+docker container list |grep ${imageName} |grep ${tag}
 
 echo "查看日志、登录容器的指令如下："
-echo "docker logs -tf --tail=100 $(docker container list |grep netease |grep 1.0 | awk '{print $1}') "
-echo "docker exec -it  $(docker container list |grep netease |grep 1.0 | awk '{print $1}') /bin/sh"
+echo "docker logs -tf --tail=100 $(docker container list |grep ${imageName} |grep ${tag} | awk '{print $1}') "
+echo "docker exec -it  $(docker container list |grep ${imageName} |grep ${tag} | awk '{print $1}') /bin/sh"
 
 echo "start finish"
